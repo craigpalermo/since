303d11c378fe0c080298ac0cb59eefc9e4666350
Craig Palermo <craig.palermo@opmbx.org>
2016-11-20 22:37:37 -0500

    Removed project files, formated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('default', () => {
-    return gulp.src('lib/*.js')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('bin'));
+  return gulp.src('lib/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('bin'));
 });
